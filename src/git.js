e463a329ef800a7ae33df3bfc1e7cd71bd291be6
Nils Petzall <nils.petzall@gmail.com>
2025-01-14 23:25:47 +0100

    fix: switch to es module

--- src/git.js
+++ src/git.js
@@ -1,76 +1,76 @@
-const { context } = require("@actions/github");
-const core = require("@actions/core");
-const { fromTag } = require("./version");
+import { context } from '@actions/github'
+import { info } from '@actions/core'
+import { fromTag } from './version'
 
 const latestRelease = async (github, tag_prefix) => {
   return github.rest.repos
     .getLatestRelease({
       owner: context.repo.owner,
-      repo: context.repo.repo,
+      repo: context.repo.repo
     })
     .catch(() => {
-      return { status: 404 };
+      return { status: 404 }
     })
     .then((response) => {
       if (response.status === 404) {
-        core.info("No release found");
-        return fromTag({ name: "0.0.0", commit: { sha: "" } });
+        info('No release found')
+        return fromTag({ name: '0.0.0', commit: { sha: '' } })
       } else {
-        core.info("Latest release tag: " + response.data.tag_name);
+        info('Latest release tag: ' + response.data.tag_name)
         return fromTag(
           {
             name: response.data.tag_name,
-            commit: { sha: response.data.target_commitish },
+            commit: { sha: response.data.target_commitish }
           },
           tag_prefix
-        );
+        )
       }
-    });
-};
+    })
+}
 
 const numberOfBumps = async (github, sha) => {
   const options = {
     owner: context.repo.owner,
-    repo: context.repo.repo,
-  };
+    repo: context.repo.repo
+  }
   if (sha && sha.length > 0) {
-    core.info("Reading commit until: " + sha);
+    info('Reading commit until: ' + sha)
   } else {
-    core.info("Reading all commits");
+    info('Reading all commits')
   }
   return github
     .paginate(github.rest.repos.listCommits, options, (response, done) => {
-      const commits = [];
+      const commits = []
       for (const commit of response.data) {
         if (commit.sha === sha) {
-          core.info("Matching commit found");
-          done();
-          break;
+          info('Matching commit found')
+          done()
+          break
         }
-        commits.push(commit);
+        commits.push(commit)
       }
-      return commits;
+      return commits
     })
     .then((commits) => {
-      core.info("Processing " + commits.length + " commits");
-      let commitCount = 0;
-      let mergeCount = 0;
-      let index = commits.length - 1;
+      info('Processing ' + commits.length + ' commits')
+      let commitCount = 0
+      let mergeCount = 0
+      let index = commits.length - 1
       while (index >= 0) {
-        const commit = commits[index];
-        commitCount++;
+        const commit = commits[index]
+        commitCount++
         if (commit.parents.length > 1) {
-          mergeCount++;
-          commitCount = 0;
+          mergeCount++
+          commitCount = 0
         }
-        index--;
+        index--
       }
-      core.info(
-        "Merges: " + mergeCount + ", post merge commits: " + commitCount
-      );
-      return mergeCount + commitCount;
-    });
-};
+      info('Merges: ' + mergeCount + ', post merge commits: ' + commitCount)
+      return mergeCount + commitCount
+    })
+}
 
-exports.latestRelease = latestRelease;
-exports.numberOfBumps = numberOfBumps;
+const _latestRelease = latestRelease
+export { _latestRelease as latestRelease }
+const _numberOfBumps = numberOfBumps
+export { _numberOfBumps as numberOfBumps }
--- src/git.test.js
+++ src/git.test.js
@@ -1,21 +1,21 @@
-const git = require("./git");
+import { latestRelease, numberOfBumps } from './git'
 
-describe("latestRelease", () => {
-  test("no tags returned", async () => {
+describe('latestRelease', () => {
+  test('no tags returned', async () => {
     const github = {
       rest: {
         repos: {
           getLatestRelease: async () => {
-            return new Promise((resolve) => resolve({ status: 404 }));
-          },
-        },
-      },
-    };
-    const version = await git.latestRelease(github);
-    expect(version.asString()).toBe("0.0.0");
-    expect(version.sha).toBe("");
-  });
-  test("release returned", async () => {
+            return new Promise((resolve) => resolve({ status: 404 }))
+          }
+        }
+      }
+    }
+    const version = await latestRelease(github)
+    expect(version.asString()).toBe('0.0.0')
+    expect(version.sha).toBe('')
+  })
+  test('release returned', async () => {
     const github = {
       rest: {
         repos: {
@@ -24,271 +24,271 @@ describe("latestRelease", () => {
               resolve({
                 status: 200,
                 data: {
-                  tag_name: "v0.0.1",
-                  target_commitish: "6c8d18338b149c8487eb2a2aa25335c273381a94",
-                },
+                  tag_name: 'v0.0.1',
+                  target_commitish: '6c8d18338b149c8487eb2a2aa25335c273381a94'
+                }
               })
-            );
-          },
-        },
-      },
-    };
-    const version = await git.latestRelease(github, "v");
-    expect(version.asString()).toBe("0.0.1");
-    expect(version.sha).toBe("6c8d18338b149c8487eb2a2aa25335c273381a94");
-  });
-});
+            )
+          }
+        }
+      }
+    }
+    const version = await latestRelease(github, 'v')
+    expect(version.asString()).toBe('0.0.1')
+    expect(version.sha).toBe('6c8d18338b149c8487eb2a2aa25335c273381a94')
+  })
+})
 
 const fiveCommits = [
   {
-    sha: "6dcb09b5b57875f334f61aebed695e2e4193db51",
+    sha: '6dcb09b5b57875f334f61aebed695e2e4193db51',
     commit: {
       tree: {
-        url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db51",
-      },
+        url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db51'
+      }
     },
     parents: [
       {
-        url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db52",
-      },
-    ],
+        url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db52'
+      }
+    ]
   },
   {
-    sha: "6dcb09b5b57875f334f61aebed695e2e4193db52",
+    sha: '6dcb09b5b57875f334f61aebed695e2e4193db52',
     commit: {
       tree: {
-        url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db52",
-      },
+        url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db52'
+      }
     },
     parents: [
       {
-        url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db53",
-      },
-    ],
+        url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db53'
+      }
+    ]
   },
   {
-    sha: "6dcb09b5b57875f334f61aebed695e2e4193db53",
+    sha: '6dcb09b5b57875f334f61aebed695e2e4193db53',
     commit: {
       tree: {
-        url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db53",
-      },
+        url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db53'
+      }
     },
     parents: [
       {
-        url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db54",
-      },
-    ],
+        url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db54'
+      }
+    ]
   },
   {
-    sha: "6dcb09b5b57875f334f61aebed695e2e4193db54",
+    sha: '6dcb09b5b57875f334f61aebed695e2e4193db54',
     commit: {
       tree: {
-        url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db54",
-      },
+        url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db54'
+      }
     },
     parents: [
       {
-        url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db55",
-      },
-    ],
+        url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db55'
+      }
+    ]
   },
   {
-    sha: "6dcb09b5b57875f334f61aebed695e2e4193db55",
+    sha: '6dcb09b5b57875f334f61aebed695e2e4193db55',
     commit: {
       tree: {
-        url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db55",
-      },
+        url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db55'
+      }
     },
     parents: [
       {
-        url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-        sha: "6dcb09b5b57875f334f61aebed695e2e4193db56",
-      },
-    ],
-  },
-];
+        url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+        sha: '6dcb09b5b57875f334f61aebed695e2e4193db56'
+      }
+    ]
+  }
+]
 
-describe("numberOfBumps", () => {
-  test("5 commits", async () => {
+describe('numberOfBumps', () => {
+  test('5 commits', async () => {
     const github = {
       paginate() {
         return new Promise((resolve) => {
-          resolve(fiveCommits);
-        });
+          resolve(fiveCommits)
+        })
       },
       rest: {
         repos: {
-          listCommits: {},
-        },
-      },
-    };
-    const bumps = await git.numberOfBumps(github, "abc");
-    expect(bumps).toBe(5);
-  });
-  test("2 merges(2+2 commits) and 1 commit", async () => {
+          listCommits: {}
+        }
+      }
+    }
+    const bumps = await numberOfBumps(github, 'abc')
+    expect(bumps).toBe(5)
+  })
+  test('2 merges(2+2 commits) and 1 commit', async () => {
     const github = {
       paginate() {
         return new Promise((resolve) => {
           resolve([
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
             },
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
                 },
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
             },
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
             },
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
             },
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
                 },
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
             },
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
             },
             {
-              sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+              sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e',
               commit: {
                 tree: {
-                  url: "https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
+                  url: 'https://api.github.com/repos/octocat/Hello-World/tree/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
               },
               parents: [
                 {
-                  url: "https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                  sha: "6dcb09b5b57875f334f61aebed695e2e4193db5e",
-                },
-              ],
-            },
-          ]);
-        });
+                  url: 'https://api.github.com/repos/octocat/Hello-World/commits/6dcb09b5b57875f334f61aebed695e2e4193db5e',
+                  sha: '6dcb09b5b57875f334f61aebed695e2e4193db5e'
+                }
+              ]
+            }
+          ])
+        })
       },
       rest: {
         repos: {
-          listCommits: {},
-        },
-      },
-    };
-    const bumps = await git.numberOfBumps(github, "");
-    expect(bumps).toBe(3);
-  });
-  test("filter by sha", async () => {
+          listCommits: {}
+        }
+      }
+    }
+    const bumps = await numberOfBumps(github, '')
+    expect(bumps).toBe(3)
+  })
+  test('filter by sha', async () => {
     const github = {
       paginate() {
-        const fn = arguments[2];
+        const fn = arguments[2]
         const results = fn(
           {
             status: 200,
-            data: fiveCommits,
+            data: fiveCommits
           },
           () => {}
-        );
+        )
         return new Promise((resolve) => {
-          resolve(results);
-        });
+          resolve(results)
+        })
       },
       rest: {
         repos: {
-          listCommits: {},
-        },
-      },
-    };
-    const bumps = await git.numberOfBumps(
+          listCommits: {}
+        }
+      }
+    }
+    const bumps = await numberOfBumps(
       github,
-      "6dcb09b5b57875f334f61aebed695e2e4193db54"
-    );
-    expect(bumps).toBe(3);
-  });
-});
+      '6dcb09b5b57875f334f61aebed695e2e4193db54'
+    )
+    expect(bumps).toBe(3)
+  })
+})
